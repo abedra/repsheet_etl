e78ee9537a57fede58e38322ecaeaf04d8ea6b75
Aaron Bedra <aaron@aaronbedra.com>
2020-09-19 22:15:38 -0500

    Add test for hash_utils

--- src/hash_utils.rs
+++ src/hash_utils.rs
@@ -7,3 +7,31 @@ pub fn create_or_increment(hash: &mut HashMap<String, i64>, key: String) {
         Occupied(mut e) => { *e.get_mut() += 1 },
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_vacant() {
+        let mut map = HashMap::<String, i64>::new();
+        create_or_increment(&mut map, "test".into());
+
+        match map.get("test") {
+            Some(&value) => assert_eq!(value, 1),
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn test_occupied() {
+        let mut map = HashMap::<String, i64>::new();
+        map.insert("test".into(), 100);
+        create_or_increment(&mut map, "test".into());
+
+        match map.get("test") {
+            Some(&value) => assert_eq!(value, 101),
+            _ => assert!(false)
+        }
+    }
+}
