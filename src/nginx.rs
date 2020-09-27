013581bbe40a1a9cb2e695b48037abd20955fd62
Aaron Bedra <aaron@aaronbedra.com>
2020-09-26 21:31:11 -0500

    Delete comments

--- src/nginx.rs
+++ src/nginx.rs
@@ -10,37 +10,6 @@ use address::Address;
 use nginx_processing_results::NginxProcessingResults;
 use file::{collect_log_files, read_log_files};
 
-// pub fn process(actors: &mut HashMap<Address, Actor>, line: &str) {
-//     let parts: Vec<&str> = line.split(' ').collect();
-//     let log_entry = LogEntry {
-//         address: Address::from(parts[0]),
-//         method: Method::from(parts[5]),
-//         response: Response::from(parts[8]),
-//     };
-//
-//     if !log_entry.method.is_valid() {
-//         match actors.entry(log_entry.address) {
-//             Vacant(key) => { key.insert(Actor { invalid_request_count: 1, ..Default::default() }); }
-//             Occupied(mut actor) => actor.get_mut().invalid_request_count += 1,
-//         }
-//         return;
-//     }
-//
-//     match actors.entry(log_entry.address) {
-//         Vacant(key) => {
-//             let mut actor = Actor { request_count: 1, ..Default::default() };
-//             actor.methods.insert(log_entry.method, 1);
-//             actor.responses.insert(log_entry.response, 1);
-//             key.insert(actor);
-//         }
-//         Occupied(mut actor) => {
-//             actor.get_mut().request_count += 1;
-//             create_or_increment(&mut actor.get_mut().methods, log_entry.method);
-//             create_or_increment(&mut actor.get_mut().responses, log_entry.response);
-//         }
-//     }
-// }
-
 #[allow(dead_code)]
 pub fn process(pattern: &str) -> NginxProcessingResults {
     let log_entries = read_log_files(collect_log_files(pattern));
